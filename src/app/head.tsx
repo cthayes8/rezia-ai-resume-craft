4e56929f410265e60514fe438124def8acb212b1
cthayes8 <130675414+cthayes8@users.noreply.github.com>
2025-05-16 13:04:52 -0400

    twitter pixel addition

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -16,6 +16,13 @@ export default function Head() {
       <meta name="twitter:title" content="Reslo – AI Resume Optimizer" />
       <meta name="twitter:description" content="Optimize your resume in seconds with AI that matches your experience to any job description. Pass ATS scans and land more interviews." />
       <meta name="twitter:image" content="/og-image.png" />
+      {/* Twitter conversion tracking base code */}
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `!function(e,t,n,s,u,a){e.twq||(s=e.twq=function(){s.exe?s.exe.apply(s,arguments):s.queue.push(arguments);},s.version='1.1',s.queue=[],u=t.createElement(n),u.async=!0,u.src='https://static.ads-twitter.com/uwt.js',a=t.getElementsByTagName(n)[0],a.parentNode.insertBefore(u,a))}(window,document,'script'); twq('config','prdxh');`
+        }}
+      />
+      {/* End Twitter conversion tracking base code */}
     </>
   );
 }
